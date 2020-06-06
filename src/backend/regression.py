b3ba33d4502fde766f33c78a5f8e6b862cb1ef27
nathanlxu <nathanlxu@gmail.com>
2020-06-06 18:40:50 -0500

    created regression model for cases vs different social distancing levels given population and density inputs

--- src/backend/regression.py
+++ src/backend/regression.py
@@ -3,60 +3,130 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import scale
 from sklearn.linear_model import LinearRegression
 
 
 class CovidRegression:
     def __init__(self):
-        self.data = utils.query_data("SELECT cases_date, state_name, cases FROM covid_state_cases")
+        self.query = "SELECT cases_date, county_state, county_name, cases FROM covid_county_cases"
+        self.data = utils.query_data(self.query)
+        self.cases = None
+        self.mobility = None
+        self.k = 10
 
-    def get_all_cases_over_time(self):
+    def process_cases_over_time(self):
         data_dict = {}
 
         for d in self.data:
-            state = d[1]
-            if state in data_dict:
-                start = data_dict[state]['start_date']
+            if 'county' in self.query:
+                name = d[1] + '-' + d[2]
+            # elif 'state' in self.query:
+            #     name = d[1]
+            else:
+                raise Exception("invalid query")
+
+            if name in data_dict:
+                start = data_dict[name]['start_date']
                 end = d[0]
                 delta = end - start
-                data_dict[state]['days'].append(delta.days + 1)
-                data_dict[state]['cases'].append(d[2])
+                data_dict[name]['days'].append(delta.days + 1)
+                data_dict[name]['cases'].append(d[-1])
             else:
-                data_dict[state] = {'days': [1], 'cases': [d[2]], 'start_date': d[0]}
+                data_dict[name] = {'days': [1], 'cases': [d[-1]], 'start_date': d[0]}
 
-        return data_dict
+        self.cases = data_dict
 
+    # process k nearest neighbors and their corresponding mobility scores into self.mobility
+    def process_mobility_scores(self, pop, den):
+        self.cases = self.cases
 
-    def plot_cases_over_time(self, pop):
-        dictionary = self.get_all_cases_over_time()
+        if 'county' in self.query:
+            population_data = utils.query_data("SELECT state, county, area_km2, population FROM counties_area_pop")
 
-        population_data = pd.read_csv('nst-est2019-alldata.csv')
-        state_populations = tuple(zip(population_data.iloc[:, 4].values, population_data.iloc[:, 16].values))
-        sorted_populations = sorted(state_populations, key=lambda x: x[1])
+            densities = np.array([pop[3] for pop in population_data]) / np.array([pop[2] for pop in population_data])
+            populations = [pop[3] for pop in population_data]
 
-        state = utils.binary_search_tuples(sorted_populations, pop)[0]
+            z_scores = (scale(np.array(densities)) + scale(np.array(populations))) / 2.0
+            standardized = list(zip([pop[0] + '-' + pop[1] for pop in population_data], z_scores))
+        # elif 'state' in self.query:
+        #     population_data = pd.read_csv('nst-est2019-alldata.csv')
+        #     # 5th column contains state names, 17th column contains population count
+        #     populations = list(zip(population_data.iloc[:, 4].values, population_data.iloc[:, 16].values))
+        else:
+            raise Exception("invalid query")
 
-        X = np.array(dictionary[state]['days']).reshape(-1, 1)
-        y = np.array(dictionary[state]['cases'])
+        sorted_populations = sorted(standardized, key=lambda x: x[1])
+
+        # standardize
+        std_pop = (pop - np.mean(np.array(populations))) / np.std(np.array(populations))
+        std_den = (den - np.mean(np.array(densities))) / np.std(np.array(densities))
+        std_score = (std_pop + std_den) / 2
+        # print("STANDARD SCORES:", std_pop, std_den, std_score)
+
+        # knn
+        county = utils.binary_search_tuples(sorted_populations, std_score)[0]
+        ind = [p[0] for p in sorted_populations].index(county)
+        nearest_neighbor_tuples = sorted_populations[ind - int(self.k/2) : ind + int(self.k/2)]
+        nearest_neighbors = [tup[0] for tup in nearest_neighbor_tuples]
+
+        start_date = '3/1/20'
+        end_date = '6/1/20'
+        mobility_scores = utils.get_mobility_data(start_date, end_date)
+        mobility_scores_nn = [(n, mobility_scores[n]) for n in nearest_neighbors if n in mobility_scores]
+
+        self.mobility = sorted(mobility_scores_nn, key=lambda x: x[1])
+
+    def create_plot(self, lvl, plt_num):
+        x = np.array(self.cases[lvl]['days']).reshape(-1, 1)
+        y = np.array(self.cases[lvl]['cases'])
 
         poly_reg = PolynomialFeatures(degree=3)
-        X_poly = poly_reg.fit_transform(X)
+        x_poly = poly_reg.fit_transform(x)
         pol_reg = LinearRegression()
-        pol_reg.fit(X_poly, y)
+        pol_reg.fit(x_poly, y)
 
-        plt.scatter(X, y, color='red')
-        plt.plot(X, pol_reg.predict(poly_reg.fit_transform(X)), color='blue')
-        plt.title('Number of Cases Over Time')
+        plot = plt.figure(plt_num)
+        plt.scatter(x, y, color='red')
+        plt.plot(x, pol_reg.predict(poly_reg.fit_transform(x)), color='blue')
+
+        t = 'Number of Cases Over Time - {} Social Distancing'
+        if plt_num == 1:
+            t = t.format("Low")
+        elif plt_num == 2:
+            t = t.format("Medium")
+        else:
+            t = t.format("High")
+
+        plt.title(t)
         plt.xlabel('Time (days)')
         plt.ylabel('Number of Cases')
 
-        t = 'Given the population parameter of {}, the spread of COVID-19 for your region would be most similar to that of {}.'
-        print(t.format(pop, state))
+        print("Regression function coefficients for figure", plt_num)
+        return pol_reg.coef_
+
+    def visualize(self):
+        # counties representing each lvl of social distancing
+        low = self.mobility[-1][0]
+        med = self.mobility[int(self.k/2)-1][0]
+        high = self.mobility[0][0]
+
+        print(self.create_plot(low, 1))
+        print(self.create_plot(med, 2))
+        print(self.create_plot(high, 3))
 
         plt.show()
 
-        return state
+        # print("fig 1:", low)
+        # print("fig 2:", med)
+        # print("fig 3:", high)
 
-population = 2000000
+
+population = 200000
+density = 500
+
 model = CovidRegression()
-model.plot_cases_over_time(population)
+model.process_cases_over_time()
+model.process_mobility_scores(population, density)
+print(model.mobility)
+model.visualize()
--- src/backend/utils.py
+++ src/backend/utils.py
@@ -1,4 +1,7 @@
 import psycopg2
+import numpy as np
+import pandas as pd
+from datetime import datetime
 
 
 def query_data(query):
@@ -14,32 +17,62 @@ def query_data(query):
     return query_results
 
 
-def count_cases_by_state(query):
+def count_cases(query):
     data = query_data(query)
-    states_data = {}
+    data_dict = {}
 
     for d in data:
-        state = d[1]
-        if state in states_data:
-            states_data[state] += 1
+        name = d[1]
+        if name in data_dict:
+            data_dict[name] += 1
         else:
-            states_data[state] = 1
+            data_dict[name] = 1
 
-    return states_data
+    return data_dict
 
 
-def binary_search_tuples(arr, x):
+def binary_search_tuples(arr, target):
     low = 0
     high = len(arr) - 1
 
     while low <= high:
         mid = (high + low) // 2
 
-        if arr[mid][1] < x:
+        if arr[mid][1] < target:
             low = mid + 1
-        elif arr[mid][1] > x:
+        elif arr[mid][1] > target:
             high = mid - 1
         else:
             return arr[mid]
 
-    return arr[low] if abs(x - arr[low][1]) < abs(x - arr[high][1]) else arr[high]
+    return arr[low] if abs(target - arr[low][1]) < abs(target - arr[high][1]) else arr[high]
+
+
+def convert_to_datetime(dates, year):
+    converted_dates = []
+    for date in dates:
+        split_date = date.split('/')
+        split_date[-1] = year
+        joined_date = '/'.join(split_date)
+        converted_dates.append(datetime.strptime(joined_date, '%m/%d/%Y'))
+
+    return converted_dates
+
+# datetime format for start, end: month/day/year - ex: 3/1/20
+def get_mobility_data(start, end):
+    df = pd.read_csv('county_mobility_data.csv')
+    start_date = start
+    end_date = end
+    county_mobility = {}
+
+    for i in range(df.shape[0]):
+        county = df.loc[i, 'region'].split(' ')
+        county.pop()
+        parsed_county = ' '.join(county)
+
+        name = df.loc[i, 'sub-region'] + '-' + parsed_county
+        mobility_data = np.array(df.loc[i, start_date:end_date])
+
+        county_mobility[name] = np.nanmean(mobility_data)
+
+    return county_mobility
